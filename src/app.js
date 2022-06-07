41f525e8b500d1d4f862e1027acaea43974d4dbf
maisienjames <maisienjames@gmail.com>
2022-06-07 23:12:05 +0100

    create artist test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const artistRouter = require('./routers/artist');
+const getDb = require('../src/services/db');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/artist', artistRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json('Hello World')
